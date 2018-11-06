33ec5afe9984905a430aaa21e9d04567c26d901e
jonathan <jonford3@hotmail.com>
2018-11-06 15:45:37 -0800

    scroll chat window to bottom

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function Messages({ currentUser, currentRoom, messages }) {
-  return (
-    <div className="messages">
-      {currentRoom
-        ? <div>
-            {messages.map((message) => {
-              return (
-                <div
-                className={message.name === currentUser
-                  ? "my-message"
-                  : "other-message"
-                }
-                key={message.id}>
-                  <span className="message chat-message">{message.message}</span>
-                  <span className="message chat-name">{message.name}</span>
-                </div>
-              )
-            })}
-          </div>
-        : ""
-      }
-    </div>
-  )
+class Messages extends React.Component {
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this);
+    this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 400 >= node.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+      const node = ReactDOM.findDOMNode(this);
+      node.scrollTop = node.scrollHeight;
+    }
+  }
+
+  render() {
+    const {
+      currentRoom,
+      messages,
+      currentUser
+    } = this.props;
+    return (
+      <div className="messages">
+        {currentRoom
+          ? <div>
+              {messages.map((message) => {
+                return (
+                  <div
+                  className={message.name === currentUser
+                    ? "my-message"
+                    : "other-message"
+                  }
+                  key={message.id}>
+                    <span className="message chat-message">{message.message}</span>
+                    <span className="message chat-name">{message.name}</span>
+                  </div>
+                )
+              })}
+            </div>
+          : ""
+        }
+      </div>
+    )
+  }
 }
 
 export default Messages;
